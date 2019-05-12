65e4f98d8ab9d773f4a88b0ac7da663c92c38725
iodar <granzowdario@gmail.com>
2019-05-12 19:57:50 +0200

    added new user dto

--- src/main/java/io/github/iodar/rest/v1/dto/NewUserDto.java
+++ src/main/java/io/github/iodar/rest/v1/dto/NewUserDto.java
@@ -3,14 +3,13 @@ package io.github.iodar.rest.v1.dto;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Data;
-import lombok.experimental.Accessors;
 
 @Data
-@Accessors(chain = true)
-@JsonPropertyOrder({"vorname", "nachname", "geburtstag"})
-public class UserDto {
-    private String vorname;
-    @JsonProperty("name")
+@JsonPropertyOrder({"nachname", "vorname", "geburtstag"})
+public class NewUserDto {
+
+    @JsonProperty(value = "name")
     private String nachname;
+    private String vorname;
     private String geburtstag;
 }
